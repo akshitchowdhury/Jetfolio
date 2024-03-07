5c344c3c53b20dec91d571fe6e99f351154cfcba
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-03-07 21:21:51 +0530

    comics1

--- src/components/Gallery/Card/Card.jsx
+++ src/components/Gallery/Card/Card.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
-import f0 from '../../../assets/gallery/kk1.jpg';
-import f1 from '../../../assets/gallery/kk2.jpg';
-import f2 from '../../../assets//gallery/kk3.jpg';
-import f3 from '../../../assets/gallery/kk4.jpg';
-import f4 from '../../../assets/gallery/kk5.jpg';
+import f0 from '../../../assets/comics/c1.jpg';
+import f1 from '../../../assets/comics/c2.jpg';
+import f2 from '../../../assets//comics/c3.webp';
+import f3 from '../../../assets/comics/c4.jpg';
+import f4 from '../../../assets/comics/c5.webp';
 // import f5 from '../../../assets/gallery/kk6.jpg';
-import f6 from '../../../assets/gallery/kk7.jpg';
-import f7 from '../../../assets/gallery/kk8.jpg';
-import f8 from '../../../assets/gallery/kk9.jpg';
-import f9 from '../../../assets/gallery/kk10.jpg';
-import f10 from '../../../assets/gallery/kk11.jpg';
-import f11 from '../../../assets/gallery/kk12.jpg';
-import f12 from '../../../assets/gallery/kk13.jpg';
-import f13 from '../../../assets/gallery/kk14.jpg';
-import f14 from '../../../assets/gallery/kk15.jpg';
-import f15 from '../../../assets/gallery/kk16.jpg';
-import f16 from '../../../assets/gallery/kk17.jpg';
+import f6 from '../../../assets/comics/c6.webp';
+import f7 from '../../../assets/comics/c7.webp';
+import f8 from '../../../assets/comics/c8.jpg';
+import f9 from '../../../assets/comics/c9.jpg';
+import f10 from '../../../assets/comics/c10.jpg';
+import f11 from '../../../assets/comics/c11.jpg';
+import f12 from '../../../assets/comics/c12.jpg';
+import f13 from '../../../assets/comics/c13.jpg';
+import f14 from '../../../assets/comics/c14.jpg';
+import f15 from '../../../assets/comics/c15.jpg';
+// import f16 from '../../../assets/gallery/kk17.jpg';
 // import f17 from '../../../assets/gallery/kk18.jpg';
 import './Card.css'
 const cardsData = [
@@ -35,7 +35,7 @@ const cardsData = [
   { image: f13, description: "Description for card 6", title: "Building tomorrow's communities with cutting-edge fabrication technology."},
   { image: f14, description: "Description for card 6", title: "Setting new benchmarks in construction for efficient labor camps."},
   { image: f15, description: "Description for card 6", title: "Revolutionizing labor accommodation through advanced fabrication techniques."},
-  { image: f16, description: "Description for card 6", title: "Crafting superior living spaces for the workforce with precision fabrication."},
+  // { image: f16, description: "Description for card 6", title: "Crafting superior living spaces for the workforce with precision fabrication."},
   // { image: f17, description: "Description for card 6", title: "Elevating standards in labor housing through innovative fabrication."},
   // { image: f18, description: "Description for card 6", title: "Pioneering solutions for labor accommodation."},
   // { image: f19, description: "Description for card 6", title: "Pioneering solutions for labor accommodation."},
@@ -48,7 +48,7 @@ const Card = () => {
     <div className='card-grid'>
       {cardsData.map((card, index) => (
         <div key={index} className="max-w-sm rounded overflow-hidden shadow-2xl m-4 card-container" >
-          <img className="w-full h-64 object-cover" src={card.image} alt={`Card ${index}`} />
+          <img className="w-full h-64 object-contain" src={card.image} alt={`Card ${index}`} />
           <div className="px-6 py-4">
             <div className="font-bold text-xl mb-2">{card.title}</div>
             {/* <p className="text-gray-700 text-base">{card.description}</p> */}
